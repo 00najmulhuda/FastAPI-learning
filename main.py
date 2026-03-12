249e9f6553c797a1c5db353674b345aae71f14ca
00najmulhuda <nh5037480@gmail.com>
2026-03-12 06:33:04 -0400

    Day 1 Path and Query Params

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 from fastapi import FastAPI
 app = FastAPI()
 
-@app.get("/leads")
-def get_leads():
-    return {"message": "this is a leads"}
 
 @app.post("/leads")
 def create_leads():
@@ -12,3 +9,11 @@ def create_leads():
 @app.delete("/leads")
 def delete_leads():
     return {"message":"delete leads"}
+
+@app.get("/leads/{lead_id}")
+def get_lead(lead_id: int):
+    return{"lead_id":lead_id}
+
+@app.get("/leads")
+def get_leads(page: int = 1):
+    return{"page":page, "message":f"page {page} leads"}
