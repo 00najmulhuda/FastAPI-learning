05f8c419b91e8b948a14e2e596a81915ce2faed7
00najmulhuda <nh5037480@gmail.com>
2026-03-12 16:04:32 -0400

    Day 2 - 3 routes with path and query params

--- main.py
+++ main.py
@@ -17,3 +17,15 @@ def get_lead(lead_id: int):
 @app.get("/leads")
 def get_leads(page: int = 1):
     return{"page":page, "message":f"page {page} leads"}
+
+@app.get("/users/{user_id}")
+def get_user(user_id:int, page:int = 1):
+    return{"user_id":user_id, "page":page}
+
+@app.get("/products/{product_id}")
+def get_product(product_id: int, category: str = "all"):
+    return {"product_id":product_id, "category":category}
+
+@app.get("/orders/{order_id}")
+def get_order(order_id: int , status: str = "pending"):
+    return{"order_id":order_id, "status":status}
