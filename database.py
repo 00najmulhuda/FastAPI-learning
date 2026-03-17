60724f4465ba7f32b09262d2599d69e0fff398e8
00najmulhuda <nh5037480@gmail.com>
2026-03-17 11:50:54 -0400

    Add databse.py engine and Session setup

--- database.py
+++ database.py
@@ -1,21 +1,8 @@
-from sqlmodel import SQLModel, Field, create_engine, Session
-
-class Company(SQLModel, table = True):
-    id:int | None = Field(default = None, primary_key = True)
-    name:str
-    typeofwork:str
-    salary:int
-
-class UserInfo(SQLModel,table=True):
-    id:int | None = Field(default = None, primary_key=True)
-    email:str
-    username:str
-    budget:int
+from sqlmodel import SQLModel, create_engine, Session
 
 DATABASE_URL = "postgresql://postgres:admin123@localhost/fastapi_learning"
 engine = create_engine(DATABASE_URL)
 
-def create_table():
-    SQLModel.metadata.create_all(engine)
-
-create_table()
+def get_session():
+    with Session(engine) as session :
+        yield session 
