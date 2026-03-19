7f14cb5ff39f9bdbbdcdc19c3f6555c6afd78edd
00najmulhuda <nh5037480@gmail.com>
2026-03-19 13:32:01 -0400

    Add schemas.py - LeadCreate and LeadResponse models

---
+++ schemas.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+from typing import Optional
+
+class LeadCreate(BaseModel):
+    name:str
+    email:str
+    company:str
+    message:str
+
+class LeadResponse(BaseModel):
+    id:int 
+    name:str
+    email:str
+    company:str
+    message:str
+    is_qualified:bool
