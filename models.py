531614974bf8145664064189c29f1123b419cdaf
00najmulhuda <nh5037480@gmail.com>
2026-04-07 13:35:33 -0400

    many-to-many relationship

--- models.py
+++ models.py
@@ -14,6 +14,12 @@ class UserInfo(SQLModel, table = True):
     budget:int
     leads:list ["Lead"] = Relationship(back_populates = "user")
 
+
+class LeadTag(SQLModel, table = True):
+    lead_id: int = Field(primary_key = True, foreign_key = "lead.id")
+    tag_id: int = Field(primary_key = True, foreign_key = "tag.id")
+
+
 class Lead(SQLModel, table = True):
     id:int | None = Field(default = None, primary_key = True)
     name:str 
@@ -23,3 +29,10 @@ class Lead(SQLModel, table = True):
     is_qualified:bool = Field(default = False)
     user_id:int = Field(foreign_key = "userinfo.id", ondelete = "CASCADE")
     user:"UserInfo" = Relationship(back_populates = "leads")
+    tags: list["Tag"] = Relationship(back_populates = "leads", link_model = LeadTag)
+
+class Tag(SQLModel, table = True):
+    id: int | None = Field(default = None, primary_key = True)
+    name: str
+    leads: list["Lead"] = Relationship(back_populates = "tags", link_model = LeadTag)
+
